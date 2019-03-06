f66c22e38418e23eeb73f822318296ead032c1eb
Roman Volkov <parrker9@gmail.com>
2019-03-06 20:31:46 +0800

    Add a simple validation for board size

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ const start = async () => {
   console.clear();
 
   console.log('Welcome to the Tic-Tac-Toe!');
-  const boardSize = await answer('Please, enter the desired size of the board:');
+  const sizeAnswer = await answer('Please, enter the desired size of the board:');
+  const boardSize = parseInt(sizeAnswer, 10);
+
+  if (isNaN(boardSize) || boardSize < 3) {
+    throw new Error('Error: For a proper game, the board size must be an integer number larger or equal to 3.');
+  }
 
   const player1Name = await answer('Enter name for Player 1:');
   const player1 = newPlayer(player1Name, 'X');
@@ -14,7 +19,7 @@ const start = async () => {
   const player2Name = await answer('Enter name for Player 2:');
   const player2 = newPlayer(player2Name, 'O');
 
-  const game = newGame(parseInt(boardSize, 10), player1, player2);
+  const game = newGame(boardSize, player1, player2);
 
   console.clear();
   game.renderBoard();
@@ -30,12 +35,17 @@ const start = async () => {
   return game;
 };
 
-start().then((game) => {
-  const winner = game.currentPlayer;
-  const message = (game.status === 'draw')
-    ? 'Game ended in a draw.'
-    : `Congratulations, ${winner.name}! You have won.`;
+start()
+  .then((game) => {
+    const winner = game.currentPlayer;
+    const message = (game.status === 'draw')
+      ? 'Game ended in a draw.'
+      : `Congratulations, ${winner.name}! You have won.`;
 
-  console.log(message);
-  process.exit(0);
-});
+    console.log(message);
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e.message);
+    process.exit(1);
+  });
