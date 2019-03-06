d02bd78b2d15aadd4d11dc2505012964bdd05f7c
Roman Volkov <parrker9@gmail.com>
2019-03-06 19:01:47 +0800

    Add tests for hasThreeInARow() method

--- src/__tests__/board.test.js
+++ src/__tests__/board.test.js
@@ -40,5 +40,85 @@ describe('#createBoard', () => {
         expect(board.slots).toEqual(['X', '2', '3', '4', '5', '6', '7', '8', '9']);
       });
     });
+
+    describe('#validSlot', () => {
+      it('returns true if the given slot does not contain a marker', () => {
+        expect(board.validSlot('1')).toBe(true);
+        expect(board.validSlot('2')).toBe(true);
+      });
+
+      it('returns false if the given slot contains a marker', () => {
+        board.set('1', 'X');
+
+        expect(board.validSlot('1')).toBe(false);
+        expect(board.validSlot('2')).toBe(true);
+      });
+    });
+
+    describe('#hasThreeInARow', () => {
+      it('returns true if there are three same markers in slots 1, 2, 3', () => {
+        board.set('1', 'X');
+        board.set('2', 'X');
+        board.set('3', 'X');
+
+        expect(board.hasThreeInARow()).toBe(true);
+      });
+
+      it('returns true if there are three same markers in slots 4, 5, 6', () => {
+        board.set('4', 'O');
+        board.set('5', 'O');
+        board.set('6', 'O');
+
+        expect(board.hasThreeInARow()).toBe(true);
+      });
+
+      it('returns true if there are three same markers in slots 7, 8, 9', () => {
+        board.set('7', 'O');
+        board.set('8', 'O');
+        board.set('9', 'O');
+
+        expect(board.hasThreeInARow()).toBe(true);
+      });
+
+      it('returns true if there are three same markers in slots 1, 4, 7', () => {
+        board.set('1', 'X');
+        board.set('4', 'X');
+        board.set('7', 'X');
+
+        expect(board.hasThreeInARow()).toBe(true);
+      });
+
+      it('returns true if there are three same markers in slots 2, 5, 8', () => {
+        board.set('2', 'O');
+        board.set('5', 'O');
+        board.set('8', 'O');
+
+        expect(board.hasThreeInARow()).toBe(true);
+      });
+
+      it('returns true if there are three same markers in slots 3, 6, 9', () => {
+        board.set('3', 'O');
+        board.set('6', 'O');
+        board.set('9', 'O');
+
+        expect(board.hasThreeInARow()).toBe(true);
+      });
+
+      it('returns true if there are three same markers in slots 1, 5, 9', () => {
+        board.set('1', 'O');
+        board.set('5', 'O');
+        board.set('9', 'O');
+
+        expect(board.hasThreeInARow()).toBe(true);
+      });
+
+      it('returns true if there are three same markers in slots 3, 5, 7', () => {
+        board.set('3', 'O');
+        board.set('5', 'O');
+        board.set('7', 'O');
+
+        expect(board.hasThreeInARow()).toBe(true);
+      });
+    });
   });
 });
